b6047f78f16a04b51c61034bc4b1535836c6ae87
ivandpf <idelpozo8@boscdelacoma.cat>
2022-10-20 18:31:54 +0200

    Canvi Php Tag

--- UF1/A3 Sessions/PHP - PHPLògic - Joc amb formularis i sessions/index.php
+++ UF1/A3 Sessions/PHP - PHPLògic - Joc amb formularis i sessions/index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="ca">
-<head>
-    <title>PHPògic</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Juga al PHPògic.">
-    <link href="//fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-</head>
-<body data-joc=<?php if (isset($_SESSION['Data'])) {echo $_SESSION['Data'];}?>>
 <?php 
 session_start();
 date_default_timezone_set('Europe/Madrid');
@@ -179,96 +168,106 @@ if (!isset($_SESSION['lletres']) || $dataAbans != $_SESSION['Data']) {
 	mirarParaula($_SESSION['funcionsPossibles'], 10);
 }
 ?>
-
-<form action="process.php" method="post">
-    <div class="main">
-        <h1>
-            <a href=""><img src="logo.png" height="54" class="logo" alt="PHPlògic"></a>
-        </h1>			
-		<?php
-			if(isset($_GET['sol']) && isset($_SESSION['lletres'])) {
-				
-				$fCorrectes = $_SESSION['funcionsCorrectes'];
-				
-				echo "
-					<p><b>SOLUCIONARI: </b></p> 
-				";
-
-				for ($i=0; $i < count($fCorrectes); $i++) { 
-					echo $fCorrectes[$i]." | ";
-				}
-			}
-		?>
-
-		<div class="container-notifications">
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <title>PHPògic</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Juga al PHPLògic.">
+    <meta name="description" content="Juga al PHPLògic.">
+    <link href="//fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body data-joc=<?php if (isset($_SESSION['Data'])) {echo $_SESSION['Data'];}?>>
+	<form action="process.php" method="post">
+		<div class="main">
+			<h1>
+				<a href=""><img src="logo.png" height="54" class="logo" alt="PHPlògic"></a>
+			</h1>			
 			<?php
+				if(isset($_GET['sol']) && isset($_SESSION['lletres'])) {
+					
+					$fCorrectes = $_SESSION['funcionsCorrectes'];
+					
+					echo "
+						<p><b>SOLUCIONARI: </b></p> 
+					";
 
-				/** 
-				* Codis d'error per $_GET. 
-				*/
-
-				if(isset($_GET['error'])) {
-					$error = $_GET['error'];
-				
-					switch ($error) {
-						case "JAHIES":
-							echo '<p class="hide" id="message">Ja hi ha la resposta.</p>';
-							break;
-						case "NOFUNCIO":
-							echo '<p class="hide" id="message">'.$_SESSION['textObtingut'].'</p>';
-							break;						
-						case "NOLLETRAMIG":
-							echo '<p class="hide" id="message">Falta la lletra del mig.</p>';
-							break;
+					for ($i=0; $i < count($fCorrectes); $i++) { 
+						echo $fCorrectes[$i]." | ";
 					}
 				}
 			?>
-        </div>
+
+			<div class="container-notifications">
+				<?php
+
+					/** 
+					* Codis d'error per $_GET. 
+					*/
+
+					if(isset($_GET['error'])) {
+						$error = $_GET['error'];
+					
+						switch ($error) {
+							case "JAHIES":
+								echo '<p class="hide" id="message">Ja hi ha la resposta.</p>';
+								break;
+							case "NOFUNCIO":
+								echo '<p class="hide" id="message">'.$_SESSION['textObtingut'].'</p>';
+								break;						
+							case "NOLLETRAMIG":
+								echo '<p class="hide" id="message">Falta la lletra del mig.</p>';
+								break;
+						}
+					}
+				?>
+			</div>
+			
+			<div class="cursor-container">
+				<p id="input-word"><span id="test-word"></span><span id="cursor">|</span></p>
+				<input type="hidden" name="pantalla" id="pantalla">
+			</div>
+			<?php 
+
+				/**
+				* Generar hexàgons.
+				*/				
+
+				echo'<div class="container-hexgrid">
+						<ul id="hex-grid">';
+				 
+				for ($i = 0; $i <= 6; $i++) {
 		
-        <div class="cursor-container">
-            <p id="input-word"><span id="test-word"></span><span id="cursor">|</span></p>
-            <input type="hidden" name="pantalla" id="pantalla">
-        </div>
-		<?php 
-
-			/**
-			* Generar hexàgons.
-			*/				
-
-			echo'<div class="container-hexgrid">
-					<ul id="hex-grid">';
-             
-            for ($i = 0; $i <= 6; $i++) {
-    
-                $tallarParaules = implode(array_slice( $_SESSION["lletres"],$i,1));            
-    
-                if ($i == 3) {
-                    echo '<li class="hex">
-                            <div class="hex-in"><a class="hex-link" data-lletra='.$tallarParaules.' id="center-letter"><p>'.$tallarParaules.'</p></a></div>
-                         </li>';
-                } else {
-                    echo '<li class="hex">
-                            <div class="hex-in"><a class="hex-link" data-lletra='.$tallarParaules.'><p>'.$tallarParaules.'</p></a></div>
-                        </li>';
-                }
-            }
-            
-			echo '</ul>
-				</div>';
-		?>
-        <div class="button-container">
-            <button id="delete-button" type="button" title="Suprimeix l'última lletra" onclick="suprimeix()"> Suprimeix</button>
-            <button id="shuffle-button" type="button" class="icon" aria-label="Barreja les lletres" title="Barreja les lletres">
-                <svg width="16" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512">
-                    <path fill="currentColor"
-                        d="M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z"></path>
-                </svg>
-            </button>
-            <button id="submit-button" type="submit" title="Introdueix la paraula">Introdueix</button>
-        </div>
-</form>
-
+					$tallarParaules = implode(array_slice( $_SESSION["lletres"],$i,1));            
+		
+					if ($i == 3) {
+						echo '<li class="hex">
+								<div class="hex-in"><a class="hex-link" data-lletra='.$tallarParaules.' id="center-letter"><p>'.$tallarParaules.'</p></a></div>
+							 </li>';
+					} else {
+						echo '<li class="hex">
+								<div class="hex-in"><a class="hex-link" data-lletra='.$tallarParaules.'><p>'.$tallarParaules.'</p></a></div>
+							</li>';
+					}
+				}
+				
+				echo '</ul>
+					</div>';
+			?>
+			<div class="button-container">
+				<button id="delete-button" type="button" title="Suprimeix l'última lletra" onclick="suprimeix()"> Suprimeix</button>
+				<button id="shuffle-button" type="button" class="icon" aria-label="Barreja les lletres" title="Barreja les lletres">
+					<svg width="16" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512 512">
+						<path fill="currentColor"
+							d="M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z"></path>
+					</svg>
+				</button>
+				<button id="submit-button" type="submit" title="Introdueix la paraula">Introdueix</button>
+			</div>
+	</form>
     <div class="scoreboard">
         <div>Has trobat <span id="letters-found"><?php if (isset($_SESSION['paraules'])){echo count($_SESSION['paraules']);} else { echo "0"; } ?></span> <span id="found-suffix"><?php if (isset($_SESSION['paraules']) && count($_SESSION['paraules']) == 1){echo "funció";} else {echo "funcions";} ?></span><span id="discovered-text">: <?php if (isset($_SESSION['paraules'])){echo ("<b>".implode(" ", $_SESSION['paraules'])."</b>");} ?></span>
         </div>
